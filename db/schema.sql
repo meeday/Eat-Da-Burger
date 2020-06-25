ca3eb2fb1e097ce8d5cbb1e207d553781d46cd27
meeday <59993824+meeday@users.noreply.github.com>
2020-06-25 11:23:17 +0100

    burgers table created

--- db/schema.sql
+++ db/schema.sql
@@ -2,4 +2,8 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-
+CREATE TABLE burgers (
+  id INT NULL PRIMARY KEY AUTO_INCREMENT,
+  burger_name VARCHAR(30) NOT NULL,
+  devoured BOOLEAN 
+);
