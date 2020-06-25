3d3c91338616f782b2edb233bb69678601876722
meeday <59993824+meeday@users.noreply.github.com>
2020-06-25 11:32:23 +0100

    table creation syntax error fixed

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-  id INT NULL PRIMARY KEY AUTO_INCREMENT,
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   burger_name VARCHAR(30) NOT NULL,
   devoured BOOLEAN 
 );
