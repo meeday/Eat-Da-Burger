5700584e4480b5c04a0a45b4f1b8d85803dc36a8
meeday <59993824+meeday@users.noreply.github.com>
2020-06-25 12:14:42 +0100

    dotenv initialised and connection objects exported

--- config/config.js
+++ config/config.js
@@ -3,6 +3,6 @@ dotenv.config();
 
 module.exports = {
   host: process.env.DB_HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
 }
