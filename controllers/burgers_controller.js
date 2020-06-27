527c7669f9ac0b0dd3e0edf1a67ced473aca56c3
meeday <59993824+meeday@users.noreply.github.com>
2020-06-27 16:54:07 +0100

    get request code created

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,15 @@
 const express = require("express");
 
 const burger = require("../models/burger");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  burger.selectAll(data => {
+    const burgerObj = {
+      burgers: data
+    };
+    console.log(burgerObj);
+    res.render("index", burgerObj);
+  });
+});
