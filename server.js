3aec70adb4607623c417d48eda03284238e7d611
meeday <59993824+meeday@users.noreply.github.com>
2020-06-27 18:25:46 +0100

    server.js file completed

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
