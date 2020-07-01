004cd47653decc524ee0f6bc9124cd855b730a54
meeday <59993824+meeday@users.noreply.github.com>
2020-07-01 18:28:06 +0100

    database env variables changed for mysql

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,11 @@
 const mysql = require("mysql");
-const dotenv = require("dotenv");
+const config = require("./config");
 
-dotenv.config();
 
-let connection;
 
-if (process.env.DATABASE_URL) {
-connection = mysql.createConnection(process.env.DATABASE_URL);
-} else {
-  connection = mysql.createConnection({
-
-    host: "localhost",
-    port:3306,
-    user: "root",
-    password: "RAVensGATE.1!",
-    database: "burgers_db"
-  });
-}
+const connection = mysql.createConnection(config.db.url);
 
 
 module.exports = connection;
+
+
