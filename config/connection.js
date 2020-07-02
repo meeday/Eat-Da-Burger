9d47503b14e110e5abb8f90422a15fd0220342e5
meeday <59993824+meeday@users.noreply.github.com>
2020-07-02 13:56:31 +0100

    connection.js comments

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,11 @@
+// require mysql package into file
 const mysql = require("mysql");
+// require config file to access db object for database connection
 const config = require("./config");
-
+// connection to mysql database created
 const connection = mysql.createConnection(config.db.url);
 
-
+// connection const exported
 module.exports = connection;
 
 
