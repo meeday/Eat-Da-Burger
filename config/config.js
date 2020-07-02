c5ff3b512cecfc5a96014ca93d605477e0386528
meeday <59993824+meeday@users.noreply.github.com>
2020-07-02 13:56:22 +0100

    config.js comments

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
+// require dotenv file
 const dotenv = require("dotenv");
-
+// initialise dotenv
 dotenv.config();
-
+// export db object containing environment variable
 module.exports = {
   db:{
     url: process.env.JAWSDB_URL,
